7c5dcf57218389534940a92b4b8e93411f3ff896
Jack McKernan <jmcker@outlook.com>
2021-04-11 13:33:28 -0400

    Fix package install

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ PROJECT_DIR="${HOME}/trace-infra"
 
 sudo apt update
 sudo apt upgrade -y
+sudo apt install -y git
 
 sudo snap install core
 sudo snap refresh core
-sudo snap install certbot docker git
+sudo snap install --classic certbot
+sudo snap install docker
 
 if [ ! -d "${PROJECT_DIR}" ]; then
     git clone https://github.com/TRACE-Digital/TRACE-infra "${PROJECT_DIR}"
