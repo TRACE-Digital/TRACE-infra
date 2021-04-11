14ded5c8224bcfcf89c6041c91d0356fcafd4f3b
Jack McKernan <jmcker@outlook.com>
2021-04-11 00:14:27 -0400

    Reload HAProxy on SSL renewal

---
+++ scripts/certbot-renew-hook.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -eo pipefail
+
+CERT_NAME="ec2"
+
+if [ "${EUID}" != "0" ]; then
+    echo "Should be run as root since it runs from the cron job!"
+    echo
+    exit 1
+fi
+
+cd "/etc/letsencrypt/live/${CERT_NAME}"
+
+# Create a combined PEM file for HAProxy
+cat "fullchain.pem" "privkey.pem" > "combined.pem"
+chmod 400 "combined.pem"
+
+# https://www.haproxy.com/documentation/hapee/2-3r1/management/starting-stopping/#reload-the-configuration
+docker exec proxy bash -c 'kill -SIGUSR2 $(cat /run/haproxy.pid)'
+
+# Can also use the runtime API to update the cert but it's a bit more complicated/fragile for our setup
+# Have to add:
+#
+# global
+#  # Enable the runtime API so we can reload SSL certs
+#  stats socket /run/haproxy-api.sock mode 600 level admin
+#
+# to haproxy.cfg
+# https://www.haproxy.com/blog/dynamic-ssl-certificate-storage-in-haproxy/
+#
+# API="nc -U /run/haproxy-api.sock"
+# CERT_CONTENTS=$(cat combined.pem)
+# # docker exec proxy bash -c "echo 'show ssl cert' | ${API}"
+# # docker exec proxy bash -c "echo 'show ssl cert /certs/combined.pem' | ${API}"
+# docker exec proxy bash -c "echo -e 'set ssl cert /certs/combined.pem <<\n${CERT_CONTENTS}\n' | ${API}"
+# # docker exec proxy bash -c "echo 'show ssl cert' | ${API}"
+# # docker exec proxy bash -c "echo 'show ssl cert */etc/haproxy/certs/site.pem' | ${API}"
+# docker exec proxy bash -c "echo 'commit ssl cert /etc/haproxy/certs/site.pem' | ${API}"
--- scripts/setup-certbot.sh
+++ scripts/setup-certbot.sh
@@ -18,11 +18,17 @@ sudo certbot certonly --standalone -n \
     --dry-run \
     --expand
 
+# Keep this inside the repository
+# Helps on a dev machine and especially on Windows since we can also
+# make 'certs' a normal folder with self-signed certs if we want
+ln -s /etc/letsencrypt/live/${CERT_NAME} ./haproxy/certs
+
 # Update certbot's HTTP port since HAProxy should now occupy 80
 # and will proxy requests for .well-known/acme-challenge here
 sed -ri.bak "s/http01_port.*/http01_port = ${CERTBOT_PORT}" "/etc/letsencrypt/renewal/${CERT_NAME}.conf"
 sed -ri "s/certbot ([^:]*):[0-9]*/certbot \1:${CERTBOT_PORT}" "./haproxy/haproxy.cfg"
 
+# Every day at 2:30 AM
 CRON_JOB="30 2 * * * /usr/bin/certbot renew --renew-hook '${PWD}/scripts/certbot-renew-hook.sh' >> /var/log/cert-renewal.log"
 
 # Maintain the existing crontab,
