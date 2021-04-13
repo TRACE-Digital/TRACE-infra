ffb81a4a17a113bdec63c2658e07ad7d86842c3b
Jack McKernan <jmcker@outlook.com>
2021-04-13 13:48:57 -0400

    Switch back to apt from snap

--- setup.sh
+++ setup.sh
@@ -5,12 +5,7 @@ PROJECT_DIR="${HOME}/trace-infra"
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y git
-
-sudo snap install core
-sudo snap refresh core
-sudo snap install --classic certbot
-sudo snap install docker
+sudo apt install -y git certbot docker.io docker-compose
 
 if [ ! -d "${PROJECT_DIR}" ]; then
     git clone https://github.com/TRACE-Digital/TRACE-infra "${PROJECT_DIR}"
